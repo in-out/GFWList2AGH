3ee9ef5554fe7e9a42950869df04f11e66490934
in-out <oneofsuns@gmail.com>
2022-06-30 22:54:53 +0800

    add quic://dns.adguard.com

--- release.sh
+++ release.sh
@@ -116,6 +116,7 @@ function GenerateRules() {
                 # "https://dns.alidns.com:443/dns-query"
                 # "https://dns.ipv6dns.com:443/dns-query"
                 # "https://doh.360.cn:443/dns-query"
+                "quic://dns.adguard.com"
                 "https://doh.pub:443/dns-query"
                 "tls://b.iqiq.io:853"
                 # "tls://dns.alidns.com:853"
